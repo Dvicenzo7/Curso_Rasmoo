b5ba7851d886a9b72958af4d79e77a0e5987db00
daniel.vicenzo <daniel.vicenzo@hypeone.com>
2025-04-02 14:28:31 -0300

    Mostrando o erro do Nullpointer e tratando

---
+++ src/main/java/TesteNull.java
@@ -0,0 +1,21 @@
+public class TesteNull {
+    public static void main(String[] args) {
+        Conta conta = new Conta();
+
+        System.out.println(conta.getSaldo());
+        System.out.println(conta.getAgencia());
+        System.out.println(conta.getNumero());
+
+
+        if(conta.getTitular() == null) {
+            conta.setTitular(new Cliente());
+        }
+
+        System.out.println(conta.getTitular()); //Vai dar Null, se nao instanciar o objeto Cliente vai dar nullpointer
+
+
+        conta.getTitular().setNome("Daniel Vicenzo");
+
+        System.out.println("O nome do titular da conta é: " + conta.getTitular().getNome());
+    }
+}
